4a44a7be3e957d6ada851677c4412b367db50c4d
Max <maxwell.meyer@gmail.com>
2021-07-17 11:33:45 -0700

    Initial Commit

---
+++ SignInFoodHunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignInFoodHunt
+//
+//  Created by Max Meyer on 7/17/21.
+//  Copyright © 2021 MeyerApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
